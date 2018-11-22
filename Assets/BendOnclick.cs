d5a89204756b3d3804330b9964a09eff649bb3c5
anaseinea <anasainea@gmail.com>
2018-11-22 16:24:49 -0600

    pushing the assets

---
+++ Assets/BendOnclick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BendOnclick : MonoBehaviour {
+
+	public Material material;
+
+	public float dampingSpeed = 0.02f, streachDistance = 2;
+
+	float streach;
+
+	
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit)) {
+				material.SetVector("_point_of_bend", hit.point);
+				streach = streachDistance;
+			}
+		}
+
+		streach = Mathf.Clamp(streach - dampingSpeed, 0, 20);
+		material.SetFloat("_streach", streach);
+	}
+}
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Project files for the tutorial video Fortnite Effect in Unity Shader Graph
 
 [Tutorial](https://youtu.be/oP7o-l9KG8I)
 
-![alt text](https://i.ytimg.com/vi/oP7o-l9KG8I/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCQAPgLpe25aFfa_qQ6aKGg7IgRZw)
+![Screenshot](http://anaseinea.github.io/Fortnite-Effect-in-Unity-Shader-Graph/Preview_image.jpg)
